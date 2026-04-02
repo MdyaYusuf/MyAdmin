0491ed6fe02d3652bd72ccae0e5ae16bde17a584
MdyaYusuf <myusufunpostakutusu@gmail.com>
2026-04-02 15:29:03 +0300

    Added security helpers and user entity.

---
+++ Api/Features/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using Api.Core.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Api.Features.Users;
+
+public class User : Entity<Guid>
+{
+  [SetsRequiredMembers]
+  public User()
+  {
+    Username = default!;
+    Email = default!;
+    PasswordHash = default!;
+    PasswordKey = default!;
+  }
+
+  public required string Username { get; set; }
+  public required string Email { get; set; }
+  public required string PasswordHash { get; set; }
+  public required string PasswordKey { get; set; }
+  public string? RefreshToken { get; set; }
+  public DateTime? RefreshTokenExpiration { get; set; }
+  public string? ProfileImageUrl { get; set; }
+  public string? Bio { get; set; }
+  public bool IsActive { get; set; } = true;
+}
